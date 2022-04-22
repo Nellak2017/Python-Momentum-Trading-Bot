e5c0d3a659ba3fb8f0057e1eee4db77bf50ffbd8
Connor Keenum <Nellakprime@gmail.com>
2022-04-21 19:57:58 -0500

    Entry Point to the application.

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ main -> analyze a given stock using a backtested algo, when the stock hits certa
 
 strategies folder -> a list of strategies to be tested;
     test -> test strategies with Unit Testing and Integration Testing ;
-    backtest -> analyze to see what is best
+    back_testing -> analyze to see what is best
 
-support folder -> all the support methods;
-    test -> test support with Unit and Integration Testing
+display folder -> all the display methods;
+    test -> test display with Unit and Integration Testing
     ema_list  -> exponential moving average data points for a given dataset and given interval and given precision
     data_collector -> collect stock data for a given stock or crypto and given interval and given precision
     data_generator -> randomly generate a time series and given interval and given precision
