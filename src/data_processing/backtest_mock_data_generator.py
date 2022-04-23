b57fa14e5b94c4265af2092f0c486cfcf4b929a2
Connor Keenum <Nellakprime@gmail.com>
2022-04-23 13:18:33 -0500

    Cleaned up docstring, removed unecessary testing code.

--- src/data_processing/backtest_mock_data_generator.py
+++ src/data_processing/backtest_mock_data_generator.py
@@ -2,26 +2,13 @@ from datetime import datetime
 import random
 import numpy as np
 
-import matplotlib.pyplot as plt
-import src.back_testing.conservative_momentum_backtest as bt
 """
-Date: 22 Apr 2022
+Date: 23 Apr 2022
 
 Author: Connor Keenum
 
 Description:
-    For this data generator, we're going to generate mock data for testing the back testers.
-
-Input:
-    ticker : String
-    initial_date : Date
-    end_date : Date
-    low_value : Float (Lowest randomly generated value)
-    high_value : Float (Highest randomly generated value)
-    step = 1 : (Optional) Int (Number of seconds between each Query)
-
-Output: (Will be displayed using strategy_display)
-    data_points : [dict] (All the position data. They have fields "ticker", "date", and "value")
+    A series of Backtest Data Generators using a variety of methods of data generation.
 
 Libraries Needed:
     Date parsing
@@ -30,8 +17,10 @@ Libraries Needed:
 """
 
 
+# ***************
 # Support Methods
-# -----------------
+# ***************
+
 def make_obj(ticker: str, date: datetime, value: float) -> dict:
     return {
         "ticker": ticker,
@@ -55,8 +44,9 @@ def brownian(mu: float = .001, sigma: float = .001, start_price: float = 1000, s
     return list(price)
 
 
-# -----------------
+# ***************
 # Data Generators
+# ***************
 
 def mock_data_generator_uniform(ticker: str, initial_date: datetime, end_date: datetime, low_value: float,
                                 high_value: float, step: int = 1) -> list:
@@ -100,7 +90,7 @@ def mock_data_generator_geometric_brownian(ticker: str, initial_date: datetime, 
     end_date_as_int: int = int(datetime.timestamp(end_date))
 
     brownian_data_points: list = brownian(mu=mu, sigma=sigma, start_price=start_price,
-                                          size=(end_date_as_int-start_date_as_int), seed=seed)
+                                          size=(end_date_as_int - start_date_as_int), seed=seed)
 
     index: int = 0
     for unix_time in range(start_date_as_int, end_date_as_int, step):
@@ -110,51 +100,3 @@ def mock_data_generator_geometric_brownian(ticker: str, initial_date: datetime, 
         index += 1
 
     return data_points
-
-# Simple test to see if it runs properly
-
-start = 1_572_014_192
-end = start + 365
-sdate = datetime.fromtimestamp(start)
-edate = datetime.fromtimestamp(end)
-ticker = "ETH"
-lowest = 2000
-query = 1  # query every second
-repeat_test = 2000
-begin_test = 500
-init_sma24 = 2000
-init_sma12 = 2030
-
-sum_ = 0
-for seed in range(begin_test, begin_test+repeat_test):
-    data_pts = mock_data_generator_geometric_brownian(ticker=ticker, initial_date=sdate, end_date=edate,
-                                                      start_price=lowest, seed=seed, mu = 0.0015)
-    back_test = bt.conservative_momentum_backtest(data_pts, init_sma24, init_sma12, r=1.10, v=.95)
-    profitability = back_test[-1]["current_profitability_multiplier"]
-    sum_ += profitability
-
-'''
-price = []
-for data in data_pts:
-    price.append(data["value"])
-
-'''
-
-print(f"Average Profitability for {repeat_test-begin_test} trials = {sum_ / repeat_test}")
-print(f"5-year profitability, {sum_ / repeat_test}^5 = {(sum_ / repeat_test)**5}")
-
-
-'''
-plt.plot(price)
-plt.show()
-
-'''
-
-
-
-
-
-
-
-
-
