cac26a27c31a4dfac311e017e9a0752e1b1da4a2
Connor Keenum <Nellakprime@gmail.com>
2022-04-24 17:08:29 -0500

    Wrote the skeleton code for this file. Will refine it further later.

---
+++ src/back_testing/simulation/conservative_momentum_single_simulation.py
@@ -0,0 +1,25 @@
+"""
+Date: 23 Apr 2022
+
+Author: Connor Keenum
+
+Description:
+    This file simulates the conservative momentum strategy using various techniques, many once.
+    The only display for this is a Chart. If you want to do this in bulk, go to the other simulation for this.
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# @Todo: Create data visualizer for all simulation methods
+# @Todo: Create a General Plotter that will include Ema12,Ema24, Buy points, Sell points, strat name, and profit
+def uniform_dist_visual():
+    pass
+
+
+def geometric_brownian_visual():
+    pass
+
+
+def real_data_visual():
+    pass
